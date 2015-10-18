254721006ed0568ec9b9572249fc569e36cf0992
asdfhamiltonian <max.s.hirsh@gmail.com>
2015-10-18 02:38:19 -0700

    added twitter

--- hyougai.py
+++ hyougai.py
@@ -4,14 +4,20 @@ This package uses the KANJIDIC dictionary file.
 This file is the property of the Electronic Dictionary Research and
 Development Group, and is used in conformance with the Group's license.
 (see http://www.csse.monash.edu.au/~jwb/kanjidic.html)
+-----
+significant parts of the twitter interface design for this bot
+were borrowed from/inspired by github user tpinecone's
+hello-world-bot
 '''
 
 import os.path
+import tweepy
+import secrets
 import random
 import pickle
 import xml.etree.ElementTree as ET
 from collections import OrderedDict
-from math import sqrt
+import time
 
 tree = ET.parse('kanjidic2.xml')
 root = tree.getroot()
@@ -102,36 +108,79 @@ if not os.path.isfile("kanjiPickle.p"):
 else:
     masterDictionary = pickle.load(open("kanjiPickle.p", "rb"))
 
-reference_list = []
-for key in masterDictionary:
-    reference = ""
+if not os.path.isfile("definitionListPickle.p"):
+    reference_list = []
+    for key in masterDictionary:
+        reference = ""
 
-    if "moro_number" in masterDictionary[key]:
-        reference += " (Morohashi " + masterDictionary[key]["moro_number"]
-    else:
-        pass
-    if "Nelson" in masterDictionary[key]:
-        reference += ", Nelson " + masterDictionary[key]["Nelson"]
-    else:
-        pass
-    if "moro_number" in masterDictionary[key]:
-        reference += ")"
+        reference += " " + key
 
-    reference += " " + key
+        for entry in masterDictionary[key]["ja_on"]:
+            reference += " " + entry + ","
+        for entry in masterDictionary[key]["ja_kun"]:
+            reference += " " + entry + ","
+        for entry in masterDictionary[key]["meaning"]:
+            reference += " " + entry + ","
+        if reference[len(reference) - 1] == ",":
+            reference = reference[:len(reference) - 1]
 
-    for entry in masterDictionary[key]["ja_on"]:
-        reference += " " + entry + ","
-    for entry in masterDictionary[key]["ja_kun"]:
-        reference += " " + entry + ","
-    for entry in masterDictionary[key]["meaning"]:
-        reference += " " + entry + ","
-    if reference[len(reference) - 1] == ",":
-        reference = reference[:len(reference) - 1]
+        if "moro_number" in masterDictionary[key]:
+            reference += " (Morohashi " + masterDictionary[key]["moro_number"]
+        else:
+            pass
+        if "Nelson" in masterDictionary[key]:
+            reference += ", Nelson " + masterDictionary[key]["Nelson"]
+        else:
+            pass
+        if "moro_number" in masterDictionary[key]:
+            reference += ")"
 
-    reference_list.append(reference)
+        reference_list.append(reference)
+    pickle.dump(reference_list, open("definitionListPickle.p", "wb"))
+else:
+    reference_list = pickle.load(open("definitionListPickle.p", "rb"))
 
-print(len(reference_list))
+"""
 a = 0
 while a < 1000:
     print(random.choice(reference_list))
     a += 1
+"""
+
+# ran an analysis on reference_list, and it turns out there are 6 items longer
+# than 140 characters. However, they only need to be split up into a maximum
+# of two tweets. Can add simple for loop before posting to account for this.
+
+def tweet(message):
+    auth = tweepy.OAuthHandler(secrets.consumer_key, secrets.consumer_secret)
+    auth.set_access_token(secrets.access_token, secrets.access_token_secret)
+    api = tweepy.API(auth)
+    auth.secure = True
+    print(len(reference_list))
+    print("Posting message {}".format(message))
+    if len(message) <= 140:
+        api.update_status(status=message)
+    elif len(message) > 140:
+        message1 = message[:140]
+        message2 = message[140:]
+        api.update_status(status=message1)
+        api.update_status(status=message2)
+    else:
+        pass
+
+
+def get_next_definition():
+    reference_list = pickle.load(open("definitionListPickle.p", "rb"))
+    message = reference_list[0]
+    del reference_list[0]
+    pickle.dump(reference_list, open("definitionListPickle.p", "wb"))
+    return message
+
+"""
+for i in range(0, 20):
+    print(get_next_definition())
+"""
+
+tweet(get_next_definition())
+
+# tweet("The quick brown fox jumps over the lazy dog. The quick brown fox jumps over the lazy dog. The quick brown fox jumps over the lazy dog. The quick brown fox...")
